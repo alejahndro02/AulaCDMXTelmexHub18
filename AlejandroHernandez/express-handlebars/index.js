9b6745064d32f5c148761bddb21813e5e926905d
alejandro <alejahndro02@gmail.com>
2018-07-11 13:52:11 -0500

    index.js handlebars

--- AlejandroHernandez/express-handlebars/index.js
+++ AlejandroHernandez/express-handlebars/index.js
@@ -1,10 +1,33 @@
-let express = require ("express")
+let express = require('express')
 let app = express()
-const path= require('path')
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname,'views'))
+const path = require('path')
 
-app.get('/', function(req, res){
-    res.render('hola')
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', function (req, res) {
+    res.render('hola', {
+        title: 'Hey! Amigos',
+        amigos: [
+            {
+                name: 'Memo',
+                age: 26,
+                hasPets: true,
+                pets: ['perro', 'gato']
+            },
+            {
+                name: 'jorge',
+                age: 48,
+                hasPets: false
+            },
+            {
+                name: 'chava',
+                age: 1000,
+                hasPets: true,
+                pets: ['changuito']
+            }
+        ]
+    })
 })
-app.listen(3000,function({console.log('connect port 3000')}))
+
+app.listen(3000, function () {console.log('Connected in port 3000')})
