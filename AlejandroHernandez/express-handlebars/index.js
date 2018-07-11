575a57fd7784b98a4fd942b8a60a0212c921c286
alejandro <alejahndro02@gmail.com>
2018-07-11 13:29:09 -0500

    insertando express-handlebars

---
+++ AlejandroHernandez/express-handlebars/index.js
@@ -0,0 +1,10 @@
+let express = require ("express")
+let app = express()
+const path= require('path')
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname,'views'))
+
+app.get('/', function(req, res){
+    res.render('hola')
+})
+app.listen(3000,function({console.log('connect port 3000')}))
